ce8d48b6d8bebf346bf1ad4b566dbed9805ed08e
DANdelion-0908 <pen22217@uvg.edu.gt>
2025-05-29 10:33:27 -0600

    Add function to fetch piece relations and add puzzle name to query

--- get.py
+++ get.py
@@ -16,7 +16,7 @@ def get_puzzle(puzzle_name: str, driver) -> list:
     )
 
     if not records:
-        print(f"Puzzle '{puzzle_name}' not found in the database.")
+        print(f"Rompecabezas '{puzzle_name}' no encontrado en la base de datos.\n")
         return None
     
     puzzle = []
@@ -35,17 +35,22 @@ def get_puzzle(puzzle_name: str, driver) -> list:
             "rel_abajo": -1
         })
 
-    print(f"Puzzle '{puzzle_name}' fetched from Neo4j database.\n")
+    print(f"Rompecabezas '{puzzle_name}' cargado desde la base de datos Neo4j.\n")
 
-    puzzle = get_piece_relations(puzzle, driver)
+    if not puzzle:
+        print(f"No se encontraron piezas para el rompecabezas '{puzzle_name}'.")
+        return None
+    
+    puzzle = get_piece_relations(puzzle, puzzle_name, driver)
     return puzzle
 
-def get_piece_relations(puzzle: list, driver) -> list:
+def get_piece_relations(puzzle: list, puzzle_name: str, driver) -> list:
     """
     Returns the relations of each piece in the puzzle.
     
     Args:
         puzzle (list): Puzzle pieces to get relations for.
+        puzzle_name (str): Name of the puzzle.
         driver: Neo4j driver instance.
 
     Returns:
@@ -54,8 +59,9 @@ def get_piece_relations(puzzle: list, driver) -> list:
     for pieza in puzzle:
         id_num = pieza["id_num"]
         records, summary, keys = driver.execute_query(
-            "MATCH (p:Pieza)-[r:CONECTADO_A]->(q:Pieza) WHERE p.id_num = $id_num RETURN p, r, q",
-            id_num=id_num,
+            "MATCH (p:Pieza {nombre_rompecabezas: $p_name})-[r:CONECTADO_A]->(q:Pieza) WHERE p.id_num = $p_id RETURN p, r, q",
+            p_id=id_num,
+            p_name=puzzle_name,
             database="neo4j"
         )
 
@@ -64,8 +70,8 @@ def get_piece_relations(puzzle: list, driver) -> list:
             relation = record["r"]
             related_piece = record["q"]
 
-            print(f"{piece["id_num"]} - {relation["direccion"]} -> {related_piece["id_num"]}")
-            
+            print(f"{piece['id_num']} - {relation['direccion']} -> {related_piece['id_num']}")
+
             if relation["direccion"] == "izquierda" and pieza["rel_izq"] == -1:
                 pieza["rel_izq"] = related_piece["id_num"]
             elif relation["direccion"] == "derecha" and pieza["rel_der"] == -1:
@@ -75,6 +81,6 @@ def get_piece_relations(puzzle: list, driver) -> list:
             elif relation["direccion"] == "abajo" and pieza["rel_abajo"] == -1:
                 pieza["rel_abajo"] = related_piece["id_num"]
 
-    print("Relations for each piece fetched from Neo4j database.\n")
+    print("Relaciones para cada pieza obtenidas de la base de datos Neo4j.\n")
 
     return puzzle
